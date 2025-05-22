3919668df3575fb968fb4d48be5e19a52b7e5034
isatest238 <isatest238@gmail.com>
2025-05-22 23:03:44 +0300

    implement xml file on local for config

--- src/test/java/Shared_Data/Shared_Data.java
+++ src/test/java/Shared_Data/Shared_Data.java
@@ -1,21 +1,32 @@
 package Shared_Data;
 
+import configFile.ConfigFile;
+import configFile.configNode.ConfigurationNode;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
 import java.time.Duration;
 
 public class Shared_Data {
+
+
+
+
+
 
     private WebDriver driver;
 
     @BeforeMethod
 
     public void prepareBrowser() {
+        ConfigurationNode configurationNode = ConfigFile.createConfigNode(ConfigurationNode.class);
+
         driver = new ChromeDriver();
-        driver.get("https://demoqa.com/");
+      //  driver.get("https://demoqa.com/");
+        driver.get(configurationNode.driverConfigNode.url);
+
+
         //definim un wait implicit pentru un interval maxim de timp
         //se activeaza dupa fiecare linie in care faci driver find element
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
