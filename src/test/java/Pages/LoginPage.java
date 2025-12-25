55fbeb3f32720d179c5d7b2c68e65b0f781abe31
Isabela Vulpe <Isabela.Vulpe@endava.com>
2025-12-25 23:18:50 +0200

    login test - page object

---
+++ src/test/java/Pages/LoginPage.java
@@ -0,0 +1,77 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPage {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    // locators
+    @FindBy(id = "onetrust-accept-btn-handler")
+    private WebElement acceptCookiesBtn;
+
+    @FindBy(id = "MENU-LOGIN")
+    private WebElement menuLoginBtn;
+
+    @FindBy(css = "input[type='email']")
+    private WebElement userInput;
+
+    @FindBy(css = "input[type='password']")
+    private WebElement passwordInput;
+
+    @FindBy(id = "pw_submit")
+    private WebElement submitBtn;
+
+    @FindBy(id = "dont_ask_again")
+    private WebElement dontAskAgainBtn;
+
+
+    public LoginPage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+        PageFactory.initElements(driver, this);
+    }
+
+    public LoginPage acceptCookies() {
+        wait.until(ExpectedConditions.elementToBeClickable(acceptCookiesBtn)).click();
+        return this;
+    }
+
+    public LoginPage clickMenuLoginBtn() {
+        wait.until(ExpectedConditions.elementToBeClickable(menuLoginBtn)).click();
+        return this;
+    }
+
+    public LoginPage enterUserInput(String user) {
+        wait.until(ExpectedConditions.elementToBeClickable(userInput)).sendKeys(user);
+        return this;
+    }
+
+    public LoginPage clickNext() {
+        wait.until(ExpectedConditions.elementToBeClickable(submitBtn)).click();
+        return this;
+    }
+
+    public LoginPage enterPassword(String password) {
+        wait.until(ExpectedConditions.visibilityOf(passwordInput)).sendKeys(password);
+        return this;
+    }
+
+    public LoginPage submitLogin() {
+        wait.until(ExpectedConditions.elementToBeClickable(submitBtn)).click();
+        return this;
+    }
+
+    public LoginPage clickDontAskAgain() {
+        wait.until(ExpectedConditions.visibilityOf(dontAskAgainBtn)).click();
+        return this;
+    }
+
+}
--- src/test/java/tests/Login_Successful.java
+++ src/test/java/tests/Login_Successful.java
@@ -1,19 +1,14 @@
 import ExtentUtility.Extent_Utility;
 import ExtentUtility.Report_Step;
 import Helper_Methods.AuthHelper;
+import Pages.LoginPage;
 import PropertyUtility.Property_Utility;
 import Shared_Data.Hooks;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
 import java.util.HashMap;
 
 public class Login_Successful extends Hooks {
@@ -32,38 +27,15 @@ public class Login_Successful extends Hooks {
                 data.get("baseUrl")
         );
 
+        LoginPage loginPage = new LoginPage(getDriver(), getWait());
+        loginPage.acceptCookies();
+        loginPage.clickMenuLoginBtn();
+        loginPage.enterUserInput(data.get("loginUser"));
+        loginPage.clickNext();
+        loginPage.enterPassword(data.get("loginPass"));
+        loginPage.submitLogin();
+        loginPage.clickDontAskAgain();
 
-        // 2) De aici începe flow-ul real din pagină (cookies + Login etc.)
-        WebElement acceptAllCookies = getWait().until(ExpectedConditions.elementToBeClickable(By.id("onetrust-accept-btn-handler")));
-        acceptAllCookies.click();
-
-
-        WebElement loginButton = getWait().until(ExpectedConditions.elementToBeClickable(By.id("MENU-LOGIN")));
-        loginButton.click();
-        Extent_Utility.attachLog(Report_Step.PASS_STEP,"Click on Login button was done with success");
-
-
-        WebElement username = getWait().until(
-                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='email']")));
-        username.sendKeys(data.get("loginUser"));
-
-        WebElement next = getWait().until(ExpectedConditions.elementToBeClickable(By.id("pw_submit")));
-        next.click();
-
-
-        WebElement password = getWait().until(
-                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='password']")));
-        password.sendKeys(data.get("loginPass"));
-
-
-        WebElement login2 = getWait().until(ExpectedConditions.elementToBeClickable(By.cssSelector("scale-button#pw_submit")));
-        login2.click();
-
-        JavascriptExecutor js = (JavascriptExecutor) getDriver();
-        js.executeScript("window.scrollBy(0, 6000);"); // 600px in jos
-
-        WebElement spater2 = getWait().until(ExpectedConditions.elementToBeClickable(By.id("dont_ask_again")));
-        spater2.click();
 
         // close the cookie pages using ESC key
         Actions actions = new Actions(getDriver());
